4c617c92ce703932af89fe5fcc3ea72698598916
szymonisokay <szymonwalach@gmail.com>
2023-11-11 19:55:43 +0100

    change user button styles

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -20,17 +20,20 @@ export const Navbar = () => {
 						elements: {
 							userButtonTrigger: 'focus:shadow-none',
 							userButtonAvatarBox: 'w-7 h-7',
-							userButtonPopoverCard: 'rounded-md shadow-xl py-4',
+							userButtonPopoverCard:
+								'rounded-md shadow-xl py-4 pt-4',
 							userPreview: 'px-4 gap-x-2',
 							userPreviewAvatarBox: 'w-9 h-9',
 							userPreviewMainIdentifier:
-								'text-slate-900 font-semibold',
-							userPreviewSecondaryIdentifier: 'text-slate-500',
+								'text-primary font-semibold',
+							userPreviewSecondaryIdentifier: 'text-secondary',
 							userButtonPopoverActions: 'mt-4',
 							userButtonPopoverActionButton: 'px-4',
-							userButtonPopoverActionButtonIcon: 'w-4 h-4',
+							userButtonPopoverActionButtonIcon:
+								'w-4 h-4 text-primary',
+							userButtonPopoverActionButtonText: 'text-primary',
 							userButtonPopoverFooter:
-								'text-slate-800 p-0 px-4 mt-4',
+								'text-secondary p-0 px-4 mt-4',
 						},
 					}}
 				/>
