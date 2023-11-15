0d549c86c638679c00ec90c5ab1bcdca5efb7e37
szymonisokay <szymonwalach@gmail.com>
2023-11-15 18:34:22 +0100

    add loading states

---
+++ app/(pages)/loading.tsx
@@ -0,0 +1,11 @@
+import { LoaderIcon } from 'lucide-react'
+
+const PagesLoading = () => {
+	return (
+		<div className='flex items-center justify-center w-full h-full'>
+			<LoaderIcon className='animate-spin' />
+		</div>
+	)
+}
+
+export default PagesLoading
